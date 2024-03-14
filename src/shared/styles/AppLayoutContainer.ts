f5a1b7431d1f900e19f1605ef7cde0f9a54d1fe1
MatheusCampos-450 <matheus.campos14@fatec.sp.gov.br>
2024-03-14 20:03:42 -0300

    feat: adjust app layout container

--- src/shared/styles/AppLayoutContainer.ts
+++ src/shared/styles/AppLayoutContainer.ts
@@ -3,10 +3,12 @@ import styled from "styled-components";
 export const AppLayoutContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
 
   flex-direction: column;
 
   width: 100%;
   height: 100%;
+
+  padding: 0 1rem;
 `;
